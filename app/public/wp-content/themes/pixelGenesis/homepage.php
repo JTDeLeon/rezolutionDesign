c0a850aa11fe4d3e7443783f471a7a1497a34a8f
Jonathan Deleon <Jondeleon856@gmail.com>
2019-08-31 08:02:08 -0400

    update homepage

---
+++ app/public/wp-content/themes/pixelGenesis/homepage.php
@@ -0,0 +1,13 @@
+<?php /**
+ * Template Name: Homepage
+ * 
+ * Pixel&Papers
+ * 
+ *
+ * @author     Jonathan Deleon <Jondeleon856@gmail.com>
+ */
+
+ ?>
+ 
+<h1>This is the homepage!!</h1>
+<h3>By Jonathan Deleon</h3>
