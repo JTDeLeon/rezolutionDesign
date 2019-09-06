e65a5601a906119b5f6a31c3917a4724f70b8912
Jonathan Deleon <Jondeleon856@gmail.com>
2019-09-06 09:59:01 -0400

    added desktop header non functional

--- app/public/wp-content/themes/pixelGenesis/content/partials/header.php
+++ app/public/wp-content/themes/pixelGenesis/content/partials/header.php
@@ -1,11 +1,51 @@
 <header>
+    <!-- Mobile Version of Header -->
     <div class="mob-header">
         <img src="wp-content/themes/pixelGenesis/content/build/image-min/header-rez-logowhite.png" alt="">
         <div class="flex">
             <img class="mauto hamburger" src="wp-content/themes/pixelGenesis/content/build/image-min/header-rez-mobile-hamburger.png" alt="">
         </div>
     </div>
+    <!-- Desktop Version of Header -->
     <div class="desk-header">
-        <h1>This is the Desktop header</h1>
+        <div class="left flex">
+            <div class="logo-container">
+                <img src="wp-content/themes/pixelGenesis/content/build/image-min/header-rez-desk-logo.png" alt="" class="logo">
+            </div>
+
+            <div class="menu-bar flex">
+                <a href="#" class="navLink">
+                    <div class="navContainer flex">
+                        <p>Services</p>
+                        <img src="wp-content/themes/pixelGenesis/content/build/image-min/header-rez-desk-downcarrot.png" alt="">
+                    </div>
+                </a>
+                <a href="#" class="navLink">
+                    <div class="navContainer flex">
+                        <p>Clients</p>
+                        <img src="wp-content/themes/pixelGenesis/content/build/image-min/header-rez-desk-downcarrot.png" alt="">
+                    </div>
+                </a>
+                <a href="#" class="navLink">
+                    <div class="navContainer flex">
+                        <p>Contact</p>
+                    </div>
+                </a>
+            </div>
+
+        </div>
+
+        <div class="right flex">
+            <div class="header-social">
+                <img src="wp-content/themes/pixelGenesis/content/build/image-min/header-rez-desk-insta.png" alt="" class="socialIcon">
+                <img src="wp-content/themes/pixelGenesis/content/build/image-min/header-rez-desk-linkin.png" alt="" class="socialIcon">
+                <img src="wp-content/themes/pixelGenesis/content/build/image-min/header-rez-desk-fb.png" alt="" class="socialIcon">
+            </div>
+
+            <a class="bookLink" href="#">
+                <img src="wp-content/themes/pixelGenesis/content/build/image-min/header-rez-desk-bookacall.png" alt="" class="book">
+            </a>
+
+        </div>
     </div>
 </header>
