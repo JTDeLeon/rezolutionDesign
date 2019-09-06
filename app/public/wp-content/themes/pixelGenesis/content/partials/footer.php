4064d5c127d8364a674e1edfdcd4b34a04e8077d
Jonathan Deleon <Jondeleon856@gmail.com>
2019-09-06 15:55:39 -0400

    update footer

--- app/public/wp-content/themes/pixelGenesis/content/partials/footer.php
+++ app/public/wp-content/themes/pixelGenesis/content/partials/footer.php
@@ -47,16 +47,43 @@
         <div class="inside-footer-container">
             <h3 class="footer-title">TECHNOLOGY PARTNERS</h3>
             <!-- First row of logos -->
-        <div class="row flex">
-            <img class="mauto mzeroleft" src="wp-content/themes/pixelGenesis/content/build/image-min/footer-rez-mob-shopifyexperts.png" alt="">
-            <img class="mauto" src="wp-content/themes/pixelGenesis/content/build/image-min/footer-rez-mob-shopifyplus.png" alt="">
-            <img class="mauto" src="wp-content/themes/pixelGenesis/content/build/image-min/footer-rez-mob-drip.png" alt="">
-            <img class="mauto" src="wp-content/themes/pixelGenesis/content/build/image-min/footer-rez-mob-klaviyc.png" alt="">
-            <img class="mauto" src="wp-content/themes/pixelGenesis/content/build/image-min/footer-rez-mob-spently.png" alt="">
-            <img class="mauto" src="wp-content/themes/pixelGenesis/content/build/image-min/footer-rez-mob-magento.png" alt="">
-            <img class="mauto" src="wp-content/themes/pixelGenesis/content/build/image-min/footer-rez-mob-woocommerce.png" alt="">
-            <img class="mauto" src="wp-content/themes/pixelGenesis/content/build/image-min/footer-rez-mob-wordpress.png" alt="">
-        </div>
+            <div class="row flex">
+                <img class="mauto mzeroleft" src="wp-content/themes/pixelGenesis/content/build/image-min/footer-rez-mob-shopifyexperts.png" alt="">
+                <img class="mauto" src="wp-content/themes/pixelGenesis/content/build/image-min/footer-rez-mob-shopifyplus.png" alt="">
+                <img class="mauto" src="wp-content/themes/pixelGenesis/content/build/image-min/footer-rez-mob-drip.png" alt="">
+                <img class="mauto" src="wp-content/themes/pixelGenesis/content/build/image-min/footer-rez-mob-klaviyc.png" alt="">
+                <img class="mauto" src="wp-content/themes/pixelGenesis/content/build/image-min/footer-rez-mob-spently.png" alt="">
+                <img class="mauto" src="wp-content/themes/pixelGenesis/content/build/image-min/footer-rez-mob-magento.png" alt="">
+                <img class="mauto" src="wp-content/themes/pixelGenesis/content/build/image-min/footer-rez-mob-woocommerce.png" alt="">
+                <img class="mauto" src="wp-content/themes/pixelGenesis/content/build/image-min/footer-rez-mob-wordpress.png" alt="">
+            </div>
+
+            <div class="bottom-sect">
+                <div class="left">
+                    <img src="wp-content/themes/pixelGenesis/content/build/image-min/footer-rez-mob-rezolutionLogo.png" alt="" class="rezolution-logo">
+                    <div class="social-bars">
+                        <a href="#"><img src="wp-content/themes/pixelGenesis/content/build/image-min/footer-rez-mob-fb.png" alt=""></a>
+                        <a href="#"><img src="wp-content/themes/pixelGenesis/content/build/image-min/footer-rez-mob-linkin.png" alt=""></a>
+                        <a href="#"><img src="wp-content/themes/pixelGenesis/content/build/image-min/footer-rez-mob-twitter.png" alt=""></a>
+                        <a href="#"><img src="wp-content/themes/pixelGenesis/content/build/image-min/footer-rez-mob-insta.png" alt=""></a>
+                    </div>
+                </div>
+                <div class="right">
+                    <div class="menuNavContainer">
+                        <div class="left mauto">
+                            <a>Why Us</a>
+                            <a>Case Studies</a>
+                            <a>Schedule a call</a>
+                        </div>
+                        <div class="right mauto">
+                            <a>Terms of Use</a>
+                            <a>Cookies Policy</a>
+                            <a>Privacy Policy</a>
+                        </div>
+                    </div>
+                    <p class="addr">18851 NE 29th Avenue —  Suite 700 / Aventura FL 33180</p>
+                </div>
+            </div>
         </div>
     </div>
 </footer>
