1cf4a04e4636ba8392d50cceb5eded8a90d1b556
Jonathan Deleon <Jondeleon856@gmail.com>
2019-09-06 08:36:58 -0400

    added mobile header and footer non functional

--- app/public/wp-content/themes/pixelGenesis/content/partials/footer.php
+++ app/public/wp-content/themes/pixelGenesis/content/partials/footer.php
@@ -1,3 +1,49 @@
 <footer>
-<h3>This is the footer</h3>
+    <div class="mob-footer">
+        <h3 class="footer-title">TECHNOLOGY PARTNERS</h3>
+        <!-- First row of logos -->
+        <div class="row flex">
+            <img class="mauto" src="wp-content/themes/pixelGenesis/content/build/image-min/footer-rez-mob-shopifyexperts.png" alt="">
+            <img class="mauto" src="wp-content/themes/pixelGenesis/content/build/image-min/footer-rez-mob-shopifyplus.png" alt="">
+            <img class="mauto" src="wp-content/themes/pixelGenesis/content/build/image-min/footer-rez-mob-drip.png" alt="">
+            <img class="mauto" src="wp-content/themes/pixelGenesis/content/build/image-min/footer-rez-mob-klaviyc.png" alt="">
+        </div>
+        <!-- Second Row of logos -->
+        <div class="row flex">
+            <img class="mauto" src="wp-content/themes/pixelGenesis/content/build/image-min/footer-rez-mob-spently.png" alt="">
+            <img class="mauto" src="wp-content/themes/pixelGenesis/content/build/image-min/footer-rez-mob-magento.png" alt="">
+            <img class="mauto" src="wp-content/themes/pixelGenesis/content/build/image-min/footer-rez-mob-woocommerce.png" alt="">
+            <img class="mauto" src="wp-content/themes/pixelGenesis/content/build/image-min/footer-rez-mob-wordpress.png" alt="">
+        </div>
+
+        <div class="flex rezLogoBottom">
+            <img src="wp-content/themes/pixelGenesis/content/build/image-min/footer-rez-mob-rezolutionLogo.png" alt="" class="mauto">
+        </div>
+
+        <div class="footerMenu flex">
+            <div class="left mauto">
+                <a>Why Us</a>
+                <a>Case Studies</a>
+                <a>Schedule a call</a>
+            </div>
+            <div class="right mauto">
+                <a>Terms of Use</a>
+                <a>Cookies Policy</a>
+                <a>Privacy Policy</a>
+            </div>
+        </div>
+
+        <div class="socialBar">
+            <a href="#"><img src="wp-content/themes/pixelGenesis/content/build/image-min/footer-rez-mob-fb.png" alt=""></a>
+            <a href="#"><img src="wp-content/themes/pixelGenesis/content/build/image-min/footer-rez-mob-linkin.png" alt=""></a>
+            <a href="#"><img src="wp-content/themes/pixelGenesis/content/build/image-min/footer-rez-mob-twitter.png" alt=""></a>
+            <a href="#"><img src="wp-content/themes/pixelGenesis/content/build/image-min/footer-rez-mob-insta.png" alt=""></a>
+        </div>
+
+        <p class="aligncenter addr">18851 NE 29th Avenue —  Suite 700 / Aventura FL 33180</p>
+    
+    </div>
+    <div class="desk-footer">
+        <h1>This is the Desktop footer</h1>
+    </div>
 </footer>
--- app/public/wp-content/themes/pixelGenesis/content/partials/header.php
+++ app/public/wp-content/themes/pixelGenesis/content/partials/header.php
@@ -1,3 +1,11 @@
 <header>
-<h1>This is the header</h1>
+    <div class="mob-header">
+        <img src="wp-content/themes/pixelGenesis/content/build/image-min/header-rez-logowhite.png" alt="">
+        <div class="flex">
+            <img class="mauto hamburger" src="wp-content/themes/pixelGenesis/content/build/image-min/header-rez-mobile-hamburger.png" alt="">
+        </div>
+    </div>
+    <div class="desk-header">
+        <h1>This is the Desktop header</h1>
+    </div>
 </header>
